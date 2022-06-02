46fe41014c244027e7bf93f992be4be444d076ab
sidharth1805 <sidharth.ramalingam@gmail.com>
2022-06-02 15:24:08 +0530

    ver 3

--- dashboard.py
+++ dashboard.py
@@ -17,3 +17,26 @@ with continent_col:
     )
 with log_x_col:
     log_x_choice = st.checkbox("Log X Axis?")
+
+df = px.data.gapminder()
+
+filtered_df = df[(df.year == year_choice)]
+
+if continent_choice != "All":
+    filtered_df = filtered_df[filtered_df.continent == continent_choice]
+
+
+fig = px.scatter(
+    filtered_df,
+    x="gdpPercap",
+    y="lifeExp",
+    size="pop",
+    color="continent",
+    hover_name="country",
+    log_x=log_x_choice,
+    size_max=60,
+)
+
+fig.update_layout(title="GDP per Capita vs. Life Expectancy")
+
+st.plotly_chart(fig, use_container_width=True)
